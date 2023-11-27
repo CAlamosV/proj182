487c59b2d00922e39fdaba2c74466adba527a3bf
Max Vogel <max-v@berkeley.edu>
2023-11-26 22:48:17 -0800

    dset

--- dataset.py
+++ dataset.py
@@ -15,42 +15,76 @@ class Dataset:
         image_width: int = 32,
         shape_side_length: int = 2,
         fps: int = 30,
-        speed_mean: float = 3.0,
-        speed_sd: float = 1.5,
-        gravity_mean: float = 0,
-        # gravity_mean: float = np.pi**2,
-        gravity_sd: float = 0,
+        # speed_mean: float = 3.0,
+        # speed_sd: float = 1.5,
+        speed_min: float = 3.0,
+        speed_max: float = 1.5,
+        # gravity_mean: float = 0,
+        # # gravity_mean: float = np.pi**2,
+        # gravity_sd: float = 0,
+        gravity_min: float = 0,
+        gravity_max: float = 0,
         restitution_min: float = 1.00,
         restitution_max: float = 1.00,
         direction_min: float = 0,
         direction_max: float = np.pi * 2,
-        position_x_mean: float = None,
-        position_x_sd: float = 2,
-        position_y_mean: float = None,
-        position_y_sd: float = 2,
+        # position_x_mean: float = None,
+        # position_x_sd: float = 2,
+        # position_y_mean: float = None,
+        # position_y_sd: float = 2,
+        # position_x_min: float = None,
+        # position_x_max: float = None,
+        # position_y_min: float = None,
+        # position_y_max: float = None,
+        position_x_delta: float = 0.0,
+        position_y_delta: float = 0.0,
         mass: float = 1.0,
         invert_colors=False,
+        seed: int = 182,
+        rt_imgs=True,
     ):
         self.image_height = image_height
         self.image_width = image_width
         self.shape_side_length = shape_side_length
         self.fps = fps  # FPS is essentially our sampling rate, and thus effectively a factor effecting the speed of the object
 
-        self.speed_mean = speed_mean
-        self.speed_sd = speed_sd
-        self.gravity_mean = gravity_mean
-        self.gravity_sd = gravity_sd
+        # self.speed_mean = speed_mean
+        # self.speed_sd = speed_sd
+        self.speed_min = speed_min
+        self.speed_max = speed_max
+        # self.gravity_mean = gravity_mean
+        # self.gravity_sd = gravity_sd
+        self.gravity_min = gravity_min
+        self.gravity_max = gravity_max
         self.restitution_min = restitution_min
         self.restitution_max = restitution_max
 
         self.direction_min = direction_min
         self.direction_max = direction_max
-        self.position_x_mean = position_x_mean or image_width / 2
-        self.position_x_sd = position_x_sd
-        self.position_y_mean = position_y_mean or image_height / 2
-        self.position_y_sd = position_y_sd
+        # self.position_x_mean = position_x_mean or image_width / 2
+        # self.position_x_sd = position_x_sd
+        # self.position_y_mean = position_y_mean or image_height / 2
+        # self.position_y_sd = position_y_sd
+        self.position_x_delta = position_x_delta
+        self.position_y_delta = position_y_delta
+        self.position_x_min = self.image_width / 2 - self.position_x_delta
+        self.position_x_max = self.image_width / 2 + self.position_x_delta
+        self.position_y_min = self.image_height / 2 - self.position_y_delta
+        self.position_y_max = self.image_height / 2 + self.position_y_delta
+        # self.position_x_min = position_x_min or 1
+        # self.position_x_max = position_x_max or image_width - 1
+        # self.position_y_min = position_y_min or 1
+        # self.position_y_max = position_y_max or image_height - 1
+        self.position_x_min = max(self.position_x_min, 1)
+        self.position_x_max = min(self.position_x_max, self.image_width - 1)
+        self.position_y_min = max(self.position_y_min, 1)
+        self.position_y_max = min(self.position_y_max, self.image_height - 1)
+
         self.mass = mass  # I'm fairly sure this doesn't matter, since we don't incorporate any rotation so momentum doesn't matter
-        self.invert_colors = invert_colors  # perhaps this impacts training?
+        self.invert_colors = invert_colors  # perhaps this impacts training? irrelevant for now (operating on coords)
+        self.seed = seed
+
+        self.rt_imgs = rt_imgs
 
     def simulate_motion(self, initial_pos, velocity, gravity, restitution):
         """
@@ -119,6 +153,7 @@ class Dataset:
         return new_pos, new_vel
 
     def draw_frame(self, position):
+        # TODO make this faster -- simply tag the position in zeroes matrix if ball width is 1
         """
         Draw a frame with the shape at the given position in black and white.
         """
@@ -186,8 +221,9 @@ class Dataset:
                 position[0],
                 self.image_height - position[1] - self.shape_side_length,
             )
-            image = self.draw_frame(adjusted_position)
-            images.append(image)
+            if self.rt_imgs:
+                image = self.draw_frame(adjusted_position)
+                images.append(image)
             positions.append(adjusted_position)
 
         images = np.asarray(images)
@@ -202,8 +238,10 @@ class Dataset:
         """
         # Sample each parameter
         initial_direction = np.random.uniform(self.direction_min, self.direction_max)
-        initial_position_x = np.random.normal(self.position_x_mean, self.position_x_sd)
-        initial_position_y = np.random.normal(self.position_y_mean, self.position_y_sd)
+        # initial_position_x = np.random.normal(self.position_x_mean, self.position_x_sd)
+        # initial_position_y = np.random.normal(self.position_y_mean, self.position_y_sd)
+        initial_position_x = np.random.uniform(self.position_x_min, self.position_x_max)
+        initial_position_y = np.random.uniform(self.position_y_min, self.position_y_max)
 
         initial_position_x = min(max(initial_position_x, 0), self.image_width)
         initial_position_y = min(max(initial_position_y, 0), self.image_height)
@@ -231,12 +269,16 @@ class Dataset:
         sequence_length=10,
         as_tensor=True,
         seed=None,
+        shuffle=False,
     ):
-        seed = seed or np.random.randint(0, 1000000)
+        seed = seed or self.seed or np.random.randint(0, 1000000)
         np.random.seed(seed)
+        torch.manual_seed(seed)
 
-        initial_speed = np.random.normal(self.speed_mean, self.speed_sd)
-        gravity = np.random.normal(self.gravity_mean, self.gravity_sd)
+        initial_speed = np.random.uniform(self.speed_min, self.speed_max)
+        # initial_speed = np.random.normal(self.speed_mean, self.speed_sd)
+        gravity = np.random.uniform(self.gravity_min, self.gravity_max)
+        # gravity = np.random.normal(self.gravity_mean, self.gravity_sd)
         coefficient_of_restitution = np.random.uniform(
             self.restitution_min, self.restitution_max
         )
@@ -248,18 +290,35 @@ class Dataset:
                 sequence_length, initial_speed, gravity, coefficient_of_restitution
             )
             # seq = np.asarray(seq)
-            if as_tensor:
-                images = torch.from_numpy(images)
-
+            # if as_tensor:
+            #     images = torch.from_numpy(images)
             sample_imgs.append(images)
             sample_xys.append(positions)
 
+        # shape (sample_cnt, sequence_length, image_height, image_width, 1)
+        sample_imgs = np.asarray(sample_imgs)
+        sample_xys = np.asarray(sample_xys)  # shape (sample_cnt, sequence_length, 2)
+
+        if shuffle:
+            indices = np.arange(sample_cnt)
+            np.random.shuffle(indices)
+            if self.rt_imgs:
+                sample_imgs = sample_imgs[indices]
+            sample_xys = sample_xys[indices]
+
+        if as_tensor:
+            sample_imgs = torch.from_numpy(sample_imgs).float()
+            sample_xys = torch.from_numpy(sample_xys).float()
+
         out = dict(
-            samples=[sample_imgs, sample_xys],
+            # samples=[sample_imgs, sample_xys],
+            xys=sample_xys,
             speed=initial_speed,
             gravity=gravity,
             restitution=coefficient_of_restitution,
         )
+        if self.rt_imgs:
+            out["imgs"] = sample_imgs
         return out
 
     def display_sequence(self, sequence):
